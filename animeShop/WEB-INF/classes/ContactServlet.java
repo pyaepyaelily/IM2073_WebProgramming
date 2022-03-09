a253301f3df35c572f4d21bacbf77f46ea565458
suga_tofu <pyaepyaelily@gmail.com>
2022-03-10 01:23:15 +0800

    solved some issues with button

--- animeShop/WEB-INF/classes/ContactServlet.java
+++ animeShop/WEB-INF/classes/ContactServlet.java
@@ -37,6 +37,25 @@ public class ContactServlet extends HttpServlet {
         out.write("      href=\"https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css\"\r\n");
         out.write("    />\r\n");
 
+        out.write("<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js\"></script>\r\n");
+        out.write("<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js\"></script>\r\n");
+
+        out.write("<link rel='stylesheet' type='text/css' href='" + request.getContextPath() + "/style.css' />");
+        out.write("<script src=\"https://code.jquery.com/jquery-3.3.1.slim.min.js\" integrity=\"sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo\" crossorigin=\"anonymous\" ></script>\r\n");
+        // out.write("<script src=\"https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js\" integrity=\"sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1\" crossorigin=\"anonymous\" ></script>\r\n");
+        
+        out.write("<script src=\"https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js\"></script>\r\n");
+        out.write("<script src=\"https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js\"></script>\r\n");
+        out.write("<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js\"></script>\r\n");
+
+        out.write("    <link\r\n");
+        out.write("      rel=\"stylesheet\"\r\n");
+        out.write("      href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\"\r\n");
+        out.write("    />\r\n");
+
+
+
+
         out.write("<link rel='stylesheet' type='text/css' href='" + request.getContextPath() +  "/contact.css' />");
         out.write("<link rel='stylesheet' type='text/css' href='" + request.getContextPath() +  "/style.css' />");
 
@@ -75,7 +94,7 @@ public class ContactServlet extends HttpServlet {
             // Step 3: Execute a SQL SELECT query
 
                 out.println("<div class='container'>");
-                out.println("<form action='action_page.php'>");
+                // out.println("<form action='action_page.php'>");
                 out.println("<label for='fname'>First Name</label>");
                 out.println("<input type='text' id='fname' name='firstname' placeholder='Your name..'>");
                 out.println("<label for='lname'>Last Name</label>");
@@ -89,11 +108,15 @@ public class ContactServlet extends HttpServlet {
                 out.println("</select>");
                 out.println("<label for='subject'>Subject</label>");
                 out.println("<textarea id='subject' name='subject' placeholder='Write something..' style='height:200px'></textarea>");
-                out.println("<input type='submit' value='Submit'>");
-                out.println("<form method='get' action='http://localhost:9999/animeShop/thankyou'>");
-                out.println("</form>");
+                out.println("<form method='get' >");
+                out.println("<button type='submit' class='btn btn-dark' style='width: 100%;' name='contactID'> Submit </button>");
+                out.println("<form />");
                 out.println("</div>");
-
+                if (request.getParameter("contactID") != null) {
+                    out.println("<script type=\"text/javascript\">");
+                    out.println("alert('Thank you for your enquiry! We will get back to you soon!');");
+                    out.println("</script>");
+                }
 
         } catch (Exception ex) {
             out.println("<p>Error: " + ex.getMessage() + "</p>");
