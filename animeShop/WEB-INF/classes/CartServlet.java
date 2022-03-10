41386266432aec03967f92833045bbcb097f225a
suga_tofu <pyaepyaelily@gmail.com>
2022-03-10 11:28:50 +0800

    modify cart to push into db

--- animeShop/WEB-INF/classes/CartServlet.java
+++ animeShop/WEB-INF/classes/CartServlet.java
@@ -44,8 +44,7 @@ public class CartServlet extends HttpServlet {
 
         out.write("<link rel='stylesheet' type='text/css' href='" + request.getContextPath() + "/style.css' />");
         out.write("<script src=\"https://code.jquery.com/jquery-3.3.1.slim.min.js\" integrity=\"sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo\" crossorigin=\"anonymous\" ></script>\r\n");
-        // out.write("<script src=\"https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js\" integrity=\"sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1\" crossorigin=\"anonymous\" ></script>\r\n");
-        
+       
         out.write("<script src=\"https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js\"></script>\r\n");
         out.write("<script src=\"https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js\"></script>\r\n");
         out.write("<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js\"></script>\r\n");
@@ -54,10 +53,6 @@ public class CartServlet extends HttpServlet {
         out.write("      rel=\"stylesheet\"\r\n");
         out.write("      href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\"\r\n");
         out.write("    />\r\n");
-
-
-
-
 
         out.println("<body>");
             out.println("<div>");
@@ -102,6 +97,7 @@ public class CartServlet extends HttpServlet {
             if (results) {
                 rsCount++;
             }
+            List<Object> orderIDList = new ArrayList<Object>();
             out.println("<br>");
             out.println("<h1 class='text-center'>Cart</h1>");
             out.println("<br>");
@@ -112,6 +108,7 @@ public class CartServlet extends HttpServlet {
                 out.println("<div class='row'>");
                     out.println("<div class='col-8 border'>");
                     while (rset.next()) {
+                        orderIDList.add(rset.getInt("productID"));
                         out.println("<div class='row'>");
                              out.println("<div class='col'>");
                              out.println("<img id='cartImg' width='100%' height='180px' src='" + request.getContextPath() + "/assets/img/" + rset.getString("img") + "' alt='image'>");
@@ -137,13 +134,12 @@ public class CartServlet extends HttpServlet {
 
                         sqlStr = "DELETE FROM cart WHERE (ID = " + id + ")";
                         count = stmt.executeUpdate(sqlStr);
+                        orderIDList.remove(id);
                         out.println("<script type=\"text/javascript\">");
                         out.println("location.replace('http://localhost:9999/animeShop/cart')");
                         out.println("</script>");
                     }
 
-
-                         
                     out.println("</div>");
                     out.println("<div class='col'>");
                         out.println("<div style='background-color: white;'>");
@@ -198,13 +194,19 @@ public class CartServlet extends HttpServlet {
                                 out.println("<form />");
 
                                 if (request.getParameter("orderID") != null) {
-                                    out.println("<script type=\"text/javascript\">");
-                                    out.println("location.replace('http://localhost:9999/animeShop/thanks')");
-                                    // out.println("alert('Thank you for your purchase!');");
-                                    out.println("</script>");
+
+                                    for (int i = 0; i < orderIDList.size(); i++) {
+                                        // out.println(orderIDList.get(i));
+                                        // out.println("test");
+                                        sqlStr = "insert into orders (customerID, id) values (1," + orderIDList.get(i) + ");";
+                                        count = stmt.executeUpdate(sqlStr);
+
+                                        out.println("<script type=\"text/javascript\">");
+                                        out.println("location.replace('http://localhost:9999/animeShop/thanks')");
+                                        // out.println("alert('Thank you for your purchase!');");
+                                        out.println("</script>");
+                                    }
                                 }
-
-
                             out.println("<br>");
                             out.println("<br>");
                             if (totalOrderValue >= 100){
